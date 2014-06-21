7fdf1ed6a6d892e6397f287655192e097d58e234
Ingolf Wagner <palipalo9@googlemail.com>
2014-06-21 22:37:59 +0200

    on fixing error

--- main/Main.hs
+++ main/Main.hs
@@ -45,15 +45,14 @@ main = do
     checkArguments opts
     let rules = rule opts
 
-    parseResult <- parseCSVFromFile (optInput opts) (optSeparator opts)
+    parseResult <- parseCSVFromFile (optSkip opts) (optInput opts) (optSeparator opts)
     case parseResult of
         Left error -> do
             hPutStrLn stderr $ error
             exitFailure
         Right csv  -> do
 
-            let toTransform = drop (optSkip opts) csv
-                actions     = toTransactions rules toTransform
+            let actions     = toTransactions rules csv
 
             updatedActions  <- case (optUpdater opts) of
                     Nothing   ->
--- src/Parser.hs
+++ src/Parser.hs
@@ -20,18 +20,25 @@ import Data.Char
 import Data.Spreadsheet
 import Control.Monad.Exception.Asynchronous.Lazy(Exceptional(..))
 import Data.List
+import System.IO.Error
+import System.IO
+
 import Qifer
+
 
 
 
 type ParseError = String
 
 -- | parse csv file to read transactions from it
-parseCSVFromFile :: String -> Char -> IO (Either ParseError CSV)
-parseCSVFromFile file separator = do
-    content <- readFile file
+parseCSVFromFile :: Int -> String -> Char -> IO (Either ParseError CSV)
+parseCSVFromFile skip file separator = do
+    firstContent <- readFile file
+    let content = concat . intersperse "\n" . drop skip . lines $ firstContent
     case (fromString '\n' separator content) of
-        Exceptional (Just s) result -> return $ Right $ strip result
+        Exceptional (Just s) result -> do
+            hPutStrLn stderr s
+            return $ Right $ strip result
         Exceptional Nothing result -> return $ Right $ strip result
 
 strip :: CSV -> CSV
