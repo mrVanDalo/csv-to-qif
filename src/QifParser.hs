d14f2ad3bc5b8c7449bd8fd2412ae6d0e14bc4c9
Jonathan Kochems <jonathan.kochems@cs.ox.ac.uk>
2015-05-04 18:51:40 +0100

    adding comments

--- src/QifParser.hs
+++ src/QifParser.hs
@@ -17,39 +17,42 @@ module QifParser where
 import QifData
 import Text.ParserCombinators.Parsec
 
-newline_skip = do _ <- newline
-                  return ()
-newline_or_eof = choice [newline_skip, eof]
-
+{- **************************************************
+    Parsers for transaction fields
+*************************************************** -}
+-- | Helper type to tag transaction field data
 data TransactionField = D String | P String | M String | T String
 
+
+-- | Parser for date field in transaction
 date_parser :: GenParser Char st TransactionField
 date_parser = do _ <- string "D"
                  date <- manyTill (noneOf ['\n','\r']) (newline_or_eof)
                  return $ D date
 
-
+-- | Parser for description field in transaction
 description_parser :: GenParser Char st TransactionField
 description_parser = do _ <- string "P"
                         description <- manyTill (noneOf ['\n','\r']) newline_or_eof
                         return $ P description
 
+-- | Parser for text field in transaction
 text_parser :: GenParser Char st TransactionField
 text_parser = do _ <- string "M"
                  text <- manyTill (noneOf ['\n','\r']) newline_or_eof
                  return $ M text
 
+-- | Parser for balance field in transaction
 balance_parser :: GenParser Char st TransactionField
 balance_parser = do _ <- string "T"
                     balance <- manyTill (noneOf ['\n','\r']) newline_or_eof
                     return $ T balance
 
+{- **************************************************
+    transactions
+*************************************************** -}
 
-type_parser :: GenParser Char st String
-type_parser = do _ <- string "!Type:"
-                 typeinfo <- manyTill (noneOf ['\n','\r']) newline_or_eof
-                 return typeinfo
-
+-- | Parser for one whole transaction
 transaction_parser :: GenParser Char st Transaction
 transaction_parser = do fields <- manyTill (choice [date_parser, description_parser, text_parser, balance_parser]) (try $ lookAhead $ seperator_parser)
                         return $ foldl fieldToTransaction 
@@ -60,27 +63,52 @@ transaction_parser = do fields <- manyTill (choice [date_parser, description_par
                     fieldToTransaction trans (M text) = trans{ text = text }
                     fieldToTransaction trans (T bal)  = trans{ balance = bal }
 
+-- | Parser for transaction seperator '^'
 seperator_parser :: GenParser Char st ()
 seperator_parser = do  _ <- string "^" 
                        _ <- newline_or_eof
                        return ()
 
-
+-- | Parser for a list of transactions
 transactions_parser :: GenParser Char st [Transaction]
 transactions_parser = sepEndBy transaction_parser seperator_parser
 
+
+{- **************************************************
+    Qif file
+*************************************************** -}
+-- | Parser for type of qif (Bank, CCard, etc.)
+type_parser :: GenParser Char st String
+type_parser = do _ <- string "!Type:"
+                 typeinfo <- manyTill (noneOf ['\n','\r']) newline_or_eof
+                 return typeinfo
+
+-- | Parser for a qif file  
 qif_file_parser :: GenParser Char st Qif
 qif_file_parser = do typeinfo     <- type_parser
                      transactions <- transactions_parser
                      return Qif{typeinfo = typeinfo, transactions = transactions }
 
-test_parser p s = parse p "(unknown)" s
-
-
-
+-- | IO Monad helper function: reads and parses qif file
+-- * if parsing successful returns qif data structure, otherwise returns "empty" qif
 parse_qif_file filename = do contents <- readFile filename
                              return $ fromRight (Qif{typeinfo = "Bank", transactions = []}) $ parse qif_file_parser "(unknown)" contents
 
+
+{- **************************************************
+    Helper functions
+*************************************************** -}
+-- | test out parser on string
+test_parser p s = parse p "(unknown)" s
+
+-- | parses newline but throws away '\n'
+newline_skip = do _ <- newline
+                  return ()
+
+-- | skips newline but also succeeds at end-of-file
+newline_or_eof = choice [newline_skip, eof]
+
+-- | quick and dirty Either exception handling with default value
 fromRight d (Right x) = x
 fromRight d _         = d
 
