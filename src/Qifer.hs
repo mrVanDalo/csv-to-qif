6d12dc109b2bd3710b88007d530ab748b3dba9a1
Ingolf Wagner <contact@ingolf-wagner.de>
2018-02-21 15:47:37 +0800

    refactored to make hlint satisfied

--- src/Qifer.hs
+++ src/Qifer.hs
@@ -11,69 +11,68 @@
 -- |
 --
 -----------------------------------------------------------------------------
-
 module Qifer where
 
-import           Data.List
-import           QifData
-import           Text.Regex.TDFA
-
+import           QifData         (Qif (..), Transaction (..), qifToLines)
+import           Text.Regex.TDFA ((=~))
 
 type CSV = [Row]
-type Row = [Column]
-type Column = String
 
+type Row = [Column]
+
+type Column = String
 
 type Position = Int
 
-data Rule = Rule {
-    dateField    :: Position
+data Rule = Rule
+  { dateField    :: Position
   , descField    :: [Position]
   , textField    :: [Position]
   , balanceField :: Position
   } deriving (Show)
 
 highestPosition :: Rule -> Position
-highestPosition (Rule dF tF ltF bF) =
-    maximum . concat $ [[dF],tF,ltF,[bF]]
+highestPosition (Rule dF tF ltF bF) = maximum . concat $ [[dF], tF, ltF, [bF]]
 
 toTransactions :: Rule -> CSV -> [Transaction]
-toTransactions _ []        = []
+toTransactions _ [] = []
 toTransactions rule (c:sv)
-    | (highestPosition rule) >= (length c) = stepDeeper
-    | otherwise                            = transform : stepDeeper
-    where pick n     = (c !! (n rule))
-          pock n     = concat . intersperse " " . map (\k -> c !! k ) $ (n rule)
-          stepDeeper = toTransactions rule sv
-          transform  = (Transaction (pick dateField)
-                                    (pock descField)
-                                    (pock textField)
-                                    (pick balanceField))
-
+  | highestPosition rule >= length c = stepDeeper
+  | otherwise = transform : stepDeeper
+  where
+    pick n = c !! n rule
+    pock n = unwords . map (\k -> c !! k) $ n rule
+    stepDeeper = toTransactions rule sv
+    transform =
+      Transaction
+        (pick dateField)
+        (pock descField)
+        (pock textField)
+        (pick balanceField)
 
 transToQif :: [Transaction] -> [String]
-transToQif trans =  qifToLines $ Qif{typeinfo = "Bank", transactions = trans }
-
+transToQif trans = qifToLines Qif {typeinfo = "Bank", transactions = trans}
 
 -- | updates a Transaction if regex works
 updateTransaction :: String -> String -> Transaction -> Transaction
 updateTransaction regex replacement transaction =
-    transaction { description = updateDesc }
-    where
-        updateDesc  = if matches then replacement else (description transaction)
-        matches     = (description transaction) =~ regex :: Bool
+  transaction {description = updateDesc}
+  where
+    updateDesc =
+      if matches
+        then replacement
+        else description transaction
+    matches = description transaction =~ regex :: Bool
 
 -- | very inefficient but better than nothing
-update :: [(String,String)] -> [Transaction] -> [Transaction]
-update _ []         = []
-update regex (t:ts) = (updateSingle regex t) : (update regex ts)
-    where
-        updateSingle [] r                                 = r
-        updateSingle ((rgx,replacement):rest) transaction =
-            if updatedTrans == transaction
-            then
-                updateSingle rest transaction
-            else
-                updatedTrans
-            where
-                updatedTrans = updateTransaction rgx replacement transaction
+update :: [(String, String)] -> [Transaction] -> [Transaction]
+update _ [] = []
+update regex (t:ts) = updateSingle regex t : update regex ts
+  where
+    updateSingle [] r = r
+    updateSingle ((rgx, replacement):rest) transaction =
+      if updatedTrans == transaction
+        then updateSingle rest transaction
+        else updatedTrans
+      where
+        updatedTrans = updateTransaction rgx replacement transaction
--- test/QifParser.hs
+++ test/QifParser.hs
@@ -8,105 +8,121 @@
 -- Stability   :  unstable
 -- Portability :
 --
--- |
+--
 --
 -----------------------------------------------------------------------------
-
 module QifParser where
 
-import QifData
-import Text.ParserCombinators.Parsec
+import           QifData
+import           Text.ParserCombinators.Parsec
 
 {- **************************************************
     Parsers for transaction fields
 *************************************************** -}
 -- | Helper type to tag transaction field data
-data TransactionField = D String | P String | M String | T String
-
+data TransactionField
+  = D String
+  | P String
+  | M String
+  | T String
 
 -- | Parser for date field in transaction
 date_parser :: GenParser Char st TransactionField
-date_parser = do _ <- string "D"
-                 date <- manyTill (noneOf ['\n','\r']) (newline_or_eof)
-                 return $ D date
+date_parser = do
+  _ <- string "D"
+  date <- manyTill (noneOf ['\n', '\r']) (newline_or_eof)
+  return $ D date
 
 -- | Parser for description field in transaction
 description_parser :: GenParser Char st TransactionField
-description_parser = do _ <- string "P"
-                        description <- manyTill (noneOf ['\n','\r']) newline_or_eof
-                        return $ P description
+description_parser = do
+  _ <- string "P"
+  description <- manyTill (noneOf ['\n', '\r']) newline_or_eof
+  return $ P description
 
 -- | Parser for text field in transaction
 text_parser :: GenParser Char st TransactionField
-text_parser = do _ <- string "M"
-                 text <- manyTill (noneOf ['\n','\r']) newline_or_eof
-                 return $ M text
+text_parser = do
+  _ <- string "M"
+  text <- manyTill (noneOf ['\n', '\r']) newline_or_eof
+  return $ M text
 
 -- | Parser for balance field in transaction
 balance_parser :: GenParser Char st TransactionField
-balance_parser = do _ <- string "T"
-                    balance <- manyTill (noneOf ['\n','\r']) newline_or_eof
-                    return $ T balance
+balance_parser = do
+  _ <- string "T"
+  balance <- manyTill (noneOf ['\n', '\r']) newline_or_eof
+  return $ T balance
 
 {- **************************************************
     transactions
 *************************************************** -}
-
 -- | Parser for one whole transaction
 transaction_parser :: GenParser Char st Transaction
-transaction_parser = do fields <- manyTill (choice [date_parser, description_parser, text_parser, balance_parser]) (try $ lookAhead $ seperator_parser)
-                        return $ foldl fieldToTransaction 
-                                       Transaction { date = "", description = "", text = "", balance = "" }
-                                       fields
-              where fieldToTransaction trans (D date) = trans{ date = date }
-                    fieldToTransaction trans (P desc) = trans{ description = desc }
-                    fieldToTransaction trans (M text) = trans{ text = text }
-                    fieldToTransaction trans (T bal)  = trans{ balance = bal }
+transaction_parser = do
+  fields <-
+    manyTill
+      (choice [date_parser, description_parser, text_parser, balance_parser])
+      (try $ lookAhead $ seperator_parser)
+  return $
+    foldl
+      fieldToTransaction
+      Transaction {date = "", description = "", text = "", balance = ""}
+      fields
+  where
+    fieldToTransaction trans (D date) = trans {date = date}
+    fieldToTransaction trans (P desc) = trans {description = desc}
+    fieldToTransaction trans (M text) = trans {text = text}
+    fieldToTransaction trans (T bal)  = trans {balance = bal}
 
 -- | Parser for transaction seperator '^'
 seperator_parser :: GenParser Char st ()
-seperator_parser = do  _ <- string "^" 
-                       _ <- newline_or_eof
-                       return ()
+seperator_parser = do
+  _ <- string "^"
+  _ <- newline_or_eof
+  return ()
 
 -- | Parser for a list of transactions
 transactions_parser :: GenParser Char st [Transaction]
 transactions_parser = sepEndBy transaction_parser seperator_parser
-
 
 {- **************************************************
     Qif file
 *************************************************** -}
 -- | Parser for type of qif (Bank, CCard, etc.)
 type_parser :: GenParser Char st String
-type_parser = do _ <- string "!Type:"
-                 typeinfo <- manyTill (noneOf ['\n','\r']) newline_or_eof
-                 return typeinfo
+type_parser = do
+  _ <- string "!Type:"
+  typeinfo <- manyTill (noneOf ['\n', '\r']) newline_or_eof
+  return typeinfo
 
 -- | Parser for a qif file
 qif_file_parser :: GenParser Char st Qif
-qif_file_parser = do typeinfo     <- type_parser
-                     transactions <- transactions_parser
-                     return Qif{typeinfo = typeinfo, transactions = transactions }
+qif_file_parser = do
+  typeinfo <- type_parser
+  transactions <- transactions_parser
+  return Qif {typeinfo = typeinfo, transactions = transactions}
 
 -- | obtain qif from string
-qifFromString s = fromRight (Qif{typeinfo = "Bank", transactions = []}) $ parse qif_file_parser "(unknown)" s
+qifFromString s =
+  fromRight (Qif {typeinfo = "Bank", transactions = []}) $
+  parse qif_file_parser "(unknown)" s
 
 -- | IO Monad helper function: reads and parses qif file
 -- * if parsing successful returns qif data structure, otherwise returns "empty" qif
-parse_qif_file filename = do contents <- readFile filename
-                             return $ qifFromString contents
-
+parse_qif_file filename = do
+  contents <- readFile filename
+  return $ qifFromString contents
 
 {- **************************************************
     Helper functions
 *************************************************** -}
 -- | test out parser on string
 -- test_parser p s = parse p "(unknown)" s
-
 -- | parses newline but throws away '\n'
-newline_skip = do _ <- newline
-                  return ()
+newline_skip = do
+  _ <- newline
+  return ()
 
 -- | skips newline but also succeeds at end-of-file
 newline_or_eof = choice [newline_skip, eof]
@@ -114,6 +130,3 @@ newline_or_eof = choice [newline_skip, eof]
 -- | quick and dirty Either exception handling with default value
 fromRight d (Right x) = x
 fromRight d _         = d
-
-
-
